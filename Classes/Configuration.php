64f4d35f3c655d99c02850fc7a5a672c3146316c
Markus Hofmann <m.hofmann@web-vision.de>
2025-06-30 10:01:31 +0200

    [WIP][FEATURE] Make DeepL translation TSconfig configurable
    Following the example of TYPO3's Page TSConfig settings, DeepL should
    also allow settings to be made via Page TSConfig. This makes it easier
    for integrators to use and provides a better overview, as the settings
    are made in the same way as for TYPO3 Core.
    
    New:
    
    ```
    mod {
        web_layout {
            localization {
                # default, set to 0 to disable DeepL Translate
                enableDeeplTranslate = 1
                # default, set to 1 to disable core translation options
                disableCoreTranslation = 0
            }
        }
    }
    ```

--- Classes/Configuration.php
+++ Classes/Configuration.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace WebVision\Deepltranslate\Core;
 
 use Symfony\Component\DependencyInjection\Attribute\AsAlias;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\MathUtility;
 
 #[AsAlias(id: ConfigurationInterface::class, public: true)]
 final class Configuration implements ConfigurationInterface
@@ -29,5 +31,45 @@ final class Configuration implements ConfigurationInterface
     public function getApiKey(): string
     {
         return $this->apiKey;
+    }
+
+    public function isDeeplTranslateAllowed(int $pageId): bool
+    {
+        $pageTsConfig = BackendUtility::getPagesTSconfig($pageId);
+        if (!is_array($pageTsConfig['mod.'] ?? null)
+            || !is_array($pageTsConfig['mod.']['web_layout.'] ?? null)
+            || !is_array($pageTsConfig['mod.']['web_layout.']['localization.'] ?? null)
+            || !(
+                is_bool($pageTsConfig['mod.']['web_layout.']['localization.']['enableDeeplTranslate'] ?? null)
+                || is_int($pageTsConfig['mod.']['web_layout.']['localization.']['enableDeeplTranslate'] ?? null)
+                || (
+                    is_string($pageTsConfig['mod.']['web_layout.']['localization.']['enableDeeplTranslate'] ?? null)
+                    && MathUtility::canBeInterpretedAsInteger($pageTsConfig['mod.']['web_layout.']['localization.']['enableDeeplTranslate'])
+                )
+            )
+        ) {
+            return true;
+        }
+        return (bool)$pageTsConfig['mod.']['web_layout.']['localization.']['enableDeeplTranslate'];
+    }
+
+    public function isCoreTranslationDisabled(int $pageId): bool
+    {
+        $pageTsConfig = BackendUtility::getPagesTSconfig($pageId);
+        if (!is_array($pageTsConfig['mod.'] ?? null)
+            || !is_array($pageTsConfig['mod.']['web_layout.'] ?? null)
+            || !is_array($pageTsConfig['mod.']['web_layout.']['localization.'] ?? null)
+            || !(
+                is_bool($pageTsConfig['mod.']['web_layout.']['localization.']['disableCoreTranslation'] ?? null)
+                || is_int($pageTsConfig['mod.']['web_layout.']['localization.']['disableCoreTranslation'] ?? null)
+                || (
+                    is_string($pageTsConfig['mod.']['web_layout.']['localization.']['disableCoreTranslation'] ?? null)
+                    && MathUtility::canBeInterpretedAsInteger($pageTsConfig['mod.']['web_layout.']['localization.']['disableCoreTranslation'])
+                )
+            )
+        ) {
+            return false;
+        }
+        return (bool)$pageTsConfig['mod.']['web_layout.']['localization.']['disableCoreTranslation'];
     }
 }
