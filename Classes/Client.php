1bdd533c57265d5111a6e78d42d6dc44dfa6ee29
Markus Hofmann <m.hofmann@web-vision.de>
2025-09-21 18:28:39 +0200

    [!!!][TASK] DPL-160: Introduce API v3 methods to `ClientInterface`
    Add the new muli lingual glossary methods to the `ClientInterface`
    and set the old ones deprecated. The deprecation is done because
    supporting API v2 and v3 does not make sense and should be skipped
    soon. Only kept until addon implementation has been aligned to this.

--- Classes/Client.php
+++ Classes/Client.php
@@ -9,6 +9,9 @@ use DeepL\GlossaryEntries;
 use DeepL\GlossaryInfo;
 use DeepL\GlossaryLanguagePair;
 use DeepL\Language;
+use DeepL\MultilingualGlossaryDictionaryEntries;
+use DeepL\MultilingualGlossaryDictionaryInfo;
+use DeepL\MultilingualGlossaryInfo;
 use DeepL\TextResult;
 use DeepL\TranslateTextOptions;
 use DeepL\Usage;
@@ -239,5 +242,68 @@ final class Client extends AbstractClient
         }
 
         return null;
+    }
+
+    /**
+     * @throws DeepLException
+     * @throws ApiKeyNotSetException
+     */
+    public function createMultilingualGlossary(string $name, array $dictionaries = []): MultilingualGlossaryInfo
+    {
+        return $this->getTranslator()->createMultilingualGlossary($name, $dictionaries);
+    }
+
+    /**
+     * @throws DeepLException
+     * @throws ApiKeyNotSetException
+     */
+    public function updateMultilingualGlossary(string $glossaryId, array $dictionaries, string $newName = ''): MultilingualGlossaryInfo
+    {
+        return $this->getTranslator()->updateMultilingualGlossary(
+            $glossaryId,
+            $newName !== '' ? $newName : null,
+            $dictionaries
+        );
+    }
+
+    /**
+     * @throws DeepLException
+     * @throws ApiKeyNotSetException
+     */
+    public function replaceMultilingualGlossary(string $glossaryId, MultilingualGlossaryDictionaryEntries $dictionaries): MultilingualGlossaryDictionaryInfo
+    {
+        return $this->getTranslator()->replaceMultilingualGlossaryDictionary($glossaryId, $dictionaries);
+    }
+
+    /**
+     * @throws DeepLException
+     * @throws ApiKeyNotSetException
+     */
+    public function deleteMultilingualGlossary(string $glossaryId): void
+    {
+        $this->getTranslator()->deleteMultilingualGlossary($glossaryId);
+    }
+
+    /**
+     * @throws DeepLException
+     * @throws ApiKeyNotSetException
+     */
+    public function listMultilingualGlossaries(): array
+    {
+        return $this->getTranslator()->listMultilingualGlossaries();
+    }
+
+    /**
+     * @throws DeepLException
+     * @throws ApiKeyNotSetException
+     */
+    public function deleteGlossaryDictionary(string $glossaryId, string $sourceLanguage, string $targetLanguage): void
+    {
+        $this->getTranslator()->deleteMultilingualGlossaryDictionary(
+            $glossaryId,
+            null,
+            $sourceLanguage,
+            $targetLanguage
+        );
     }
 }
