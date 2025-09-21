ea61d36adad89434eff0379be4780a40b14c4c90
Markus Hofmann <m.hofmann@web-vision.de>
2025-09-21 13:20:02 +0200

    [BUGFIX] Ensure correct formatting in PHP 8.4
    Since php 8.4 the ext-intl class `NumberFormatter` expects a strict
    locale string and throws a `ValueError` if the given value cannot get
    parsed.
    
    Together with the `uc` of TYPO3, which holds the backend language for
    the currently logged-in user, this could be set to `default`, which is
    no valid locale. New TYPO3 instances will write a `uc['lang']=''`, if
    the person uses `englisch`, which is the default, but older used to
    write `us['lang']='default'`, which is still correct.
    
    This throws the `ValueError` if the NumberFormatter should format with
    this setting.
    
    The Locales usage is not used here (other than on #491), as TYPO3 11
    does not provide the necessary functions here.
    
    Fixes #486

--- Tests/Functional/Services/UsageServiceTest.php
+++ Tests/Functional/Services/UsageServiceTest.php
@@ -6,6 +6,7 @@ namespace WebVision\WvDeepltranslate\Tests\Functional\Services;
 
 use DeepL\Usage;
 use DeepL\UsageDetail;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use WebVision\WvDeepltranslate\Service\DeeplService;
 use WebVision\WvDeepltranslate\Service\ProcessingInstruction;
 use WebVision\WvDeepltranslate\Service\UsageService;
@@ -112,5 +113,45 @@ final class UsageServiceTest extends AbstractDeepLTestCase
         $character = $usage->character;
         static::assertInstanceOf(UsageDetail::class, $character);
         static::assertEquals(strlen($translateContent), $character->count);
+    }
+
+    public static function numberFormatterLocalesDataProvider(): \Generator
+    {
+        yield 'Default formats to english' => [
+            'user' => 1,
+            'number' => 20000,
+            'expectedFormat' => '20,000',
+        ];
+        yield 'BE uc lang "de" formats german' => [
+            'user' => 2,
+            'number' => 93254850,
+            'expectedFormat' => '93.254.850',
+        ];
+    }
+    /**
+     * This test ensures that in PHP >=8.4 the NumberFormatter works correctly.
+     * With migrated TYPO3 data there is the possibility that uc['lang'] is set to 'default',
+     * which is no correct format for a locale the number formatter accepts. THis will lead
+     * to an error during initialisation.
+     *
+     * @test
+     * @dataProvider numberFormatterLocalesDataProvider
+     */
+    public function numberFormatRespectsLocalesAndDefault(
+        int $user,
+        int $number,
+        string $expectedFormat
+    ): void {
+        // TYPO3 v11 seems not to respect the UC language setting, if other than default. skip this test for user 2
+        if ((new Typo3Version())->getMajorVersion() <= 11 && $user === 2) {
+            static::markTestSkipped('The issue only appears in PHP 8.4, which is not supported with TYPO3 11');
+        }
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/Pages.csv');
+        $this->setUpBackendUser($user);
+        /** @var UsageService $usageService */
+        $usageService = $this->get(UsageService::class);
+
+        $formatted = $usageService->formatNumber($number);
+        static::assertEquals($expectedFormat, $formatted);
     }
 }
