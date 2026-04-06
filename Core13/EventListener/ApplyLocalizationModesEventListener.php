5d1ed127bee835031516ce7fb6df3088a6199505
Stefan Bürk <stefan@buerk.tech>
2026-04-06 15:12:18 +0200

    [!!!][TASK] DPL-158: Remove `autotranslate` translation mode
    The `autotranslate` mode has been removed now because it
    literally was only a duplicate of the normal `translate`
    mode and was not handled differently.
    
    Related translation keys and TYPO3 v12 action icon svg
    is removed as being obsolete now.
    
    Used command(s):
    
    ```bash
    Build/Scripts/runTests.sh -t 13 -s phpstanGenerateBaseline
    ```
    
    Resolves: #498

--- Core13/EventListener/ApplyLocalizationModesEventListener.php
+++ Core13/EventListener/ApplyLocalizationModesEventListener.php
@@ -6,7 +6,6 @@ namespace WebVision\Deepltranslate\Core\Core13\EventListener;
 
 use TYPO3\CMS\Core\Attribute\AsEventListener;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
-use TYPO3\CMS\Core\Information\Typo3Version;
 use WebVision\Deepl\Base\Core13\Controller\Backend\LocalizationController;
 use WebVision\Deepl\Base\Event\GetLocalizationModesEvent;
 use WebVision\Deepl\Base\Localization\LocalizationMode;
@@ -34,42 +33,24 @@ final class ApplyLocalizationModesEventListener
             return;
         }
         $modes = [];
-        $majorVersion = (new Typo3Version())->getMajorVersion();
         if ($this->allowDeeplTranslate($event)) {
             $modes[] = new LocalizationMode(
                 identifier: 'deepltranslate',
                 title: $event->getLanguageService()->sL('LLL:EXT:deepltranslate_core/Resources/Private/Language/locallang.xlf:localize.educate.deepltranslateHeader'),
                 description: $event->getLanguageService()->sL('LLL:EXT:deepltranslate_core/Resources/Private/Language/locallang.xlf:localize.educate.deepltranslate'),
-                icon: ($majorVersion === 13 ? 'actions-localize-deepl-13' : 'actions-localize-deepl'),
-                before: [],
-                after: [LocalizationController::ACTION_LOCALIZE, LocalizationController::ACTION_COPY],
-            );
-        }
-        if ($this->allowDeeplTranslateAuto($event)) {
-            // @todo Consider to drop `deepltranslateauto` mode. Does not make much sense in the current form.
-            $modes[] = new LocalizationMode(
-                identifier: 'deepltranslateauto',
-                title: $event->getLanguageService()->sL('LLL:EXT:deepltranslate_core/Resources/Private/Language/locallang.xlf:localize.educate.deepltranslateHeaderAutodetect'),
-                description: $event->getLanguageService()->sL('LLL:EXT:deepltranslate_core/Resources/Private/Language/locallang.xlf:localize.educate.deepltranslateAuto'),
-                icon: ($majorVersion === 13 ? 'actions-localize-deepl-13' : 'actions-localize-deepl'),
+                icon: 'actions-localize-deepl-13',
                 before: [],
                 after: [LocalizationController::ACTION_LOCALIZE, LocalizationController::ACTION_COPY],
             );
         }
         if ($modes !== []) {
-            $event->getModes()->add(...array_values($modes));
+            $event->getModes()->add(...$modes);
         }
     }
 
     private function allowDeeplTranslate(GetLocalizationModesEvent $event): bool
     {
         // @todo Prepared for PageTSConfig feature to toggle `deepltranslate`.
-        return true;
-    }
-
-    private function allowDeeplTranslateAuto(GetLocalizationModesEvent $event): bool
-    {
-        // @todo Prepared for PageTSConfig feature to toggle `deepltranslateauto`.
         return true;
     }
 
