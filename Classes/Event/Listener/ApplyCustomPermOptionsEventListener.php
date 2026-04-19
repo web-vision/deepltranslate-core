e17be35ea08b03531c5db2f60b234215fc4a3b94
Stefan Bürk <stefan@buerk.tech>
2026-04-19 21:37:36 +0200

    [TASK] DPL-158: Move `customPermOption` apply to event
    `EXT:deepltranslate_core` provides a custom access permission
    registry gathered in extension in `ext_localconf.php` files
    and applied in `ext_tables.php` to have all set.
    
    `ext_tables.php` is deprecated since TYPO3 v14.3 and current
    solution spams deprecation message in environments and let
    functional tests fail.
    
    This change moves building `customPermOptions.deepltranslate`
    from `ext_tables.php` into a new PSR-14 EventListener for the
    `\TYPO3\CMS\Core\Core\Event\BootCompletedEvent` event to do
    the same job and avoiding the TYPO3 v14 deprecation. The event
    is dispatched slightly before including `ext_tables.php`.
    
    Empty `ext_tables.php` file is removed directly.
    
    [1] https://review.typo3.org/c/Packages/TYPO3.CMS/+/93488
    [2] https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/14.3/Deprecation-109438-ExtTablesPhpInExtensions.html

---
+++ Classes/Event/Listener/ApplyCustomPermOptionsEventListener.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebVision\Deepltranslate\Core\Event\Listener;
+
+use TYPO3\CMS\Core\Attribute\AsEventListener;
+use TYPO3\CMS\Core\Core\Event\BootCompletedEvent;
+use WebVision\Deepltranslate\Core\Access\AccessRegistry;
+
+/**
+ * Listen to {@see BootCompletedEvent} to apply all registered deepltranslate `customPermOptions`
+ * gathered through {@see AccessRegistry::addAccess()} calls in extension `ext_localconf.php` files.
+ */
+final class ApplyCustomPermOptionsEventListener
+{
+    public function __construct(
+        private AccessRegistry $accessRegistry,
+    ) {}
+
+    #[AsEventListener(identifier: 'deepltranslate-core/apply-custom-perm-options-deepltranslate')]
+    public function __invoke(BootCompletedEvent $event): void
+    {
+        $GLOBALS['TYPO3_CONF_VARS']['BE']['customPermOptions']['deepltranslate'] ??= [];
+        $GLOBALS['TYPO3_CONF_VARS']['BE']['customPermOptions']['deepltranslate']['header'] = 'Deepl Translate Access';
+        foreach ($this->accessRegistry->getAllAccess() as $access) {
+            $GLOBALS['TYPO3_CONF_VARS']['BE']['customPermOptions']['deepltranslate']['items'][$access->getIdentifier()] = [
+                $access->getTitle(),
+                $access->getIconIdentifier(),
+                $access->getDescription(),
+            ];
+        }
+    }
+}
