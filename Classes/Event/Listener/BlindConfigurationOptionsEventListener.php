24a2cca40fe2f08851f77ec67b62a52f168e05e3
André Buchmann <andy.schliesser@gmail.com>
2026-05-26 21:20:49 +0200

    [TASK] Blind configuration options
    Hide sensitive data in configuration module using
    the `ModifyBlindedConfigurationOptionsEvent`.

---
+++ Classes/Event/Listener/BlindConfigurationOptionsEventListener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace WebVision\Deepltranslate\Core\Event\Listener;
+
+use TYPO3\CMS\Core\Attribute\AsEventListener;
+use TYPO3\CMS\Lowlevel\Event\ModifyBlindedConfigurationOptionsEvent;
+
+/**
+ * Listen to {@see ModifyBlindedConfigurationOptionsEvent} to hide sensitive data
+ * in the configuration module.
+ */
+final readonly class BlindConfigurationOptionsEventListener
+{
+    #[AsEventListener(
+        identifier: 'deepltranslate-core/blind-configuration-options',
+    )]
+    public function __invoke(ModifyBlindedConfigurationOptionsEvent $event): void
+    {
+        $options = $event->getBlindedConfigurationOptions();
+
+        if ($event->getProviderIdentifier() === 'confVars') {
+            $options['TYPO3_CONF_VARS']['EXTENSIONS']['deepltranslate_core']['apiKey'] = '******';
+        }
+
+        $event->setBlindedConfigurationOptions($options);
+    }
+}
