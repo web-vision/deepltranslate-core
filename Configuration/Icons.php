6445750d7e2dfc878ab4e489d1507300bb79c655
Stefan Bürk <stefan@buerk.tech>
2026-06-05 04:40:47 +0200

    [TASK] DPL-158: Provide light/dark theme aware action and logo icon
    This change uses now the `DeeplBaseSvgIconProvider` provided by the
    `EXT:deepl_base` extension to register `light|dark theme` aware
    action icons and logo icons in the `Configuration/Icons.php` file.
    
    That allows `EXT:deepltranslate_core` directly and also addons to
    use suiting icons for example on buttons while beeing color scheme
    aware.

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -3,15 +3,16 @@
 declare(strict_types=1);
 
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+use WebVision\Deepl\Base\Imaging\IconProvider\DeeplBaseSvgIconProvider;
 
 return [
     'actions-localize-deepl-13' => [
-        'provider' => SvgIconProvider::class,
-        'source' => 'EXT:deepltranslate_core/Resources/Public/Icons/actions-localize-deepl-v13.svg',
+        'provider' => DeeplBaseSvgIconProvider::class,
+        'source' => 'EXT:deepltranslate_core/Resources/Public/Icons/deepl-mode-aware.svg',
     ],
     'actions-localize-deepl-14' => [
-        'provider' => SvgIconProvider::class,
-        'source' => 'EXT:deepltranslate_core/Resources/Public/Icons/actions-localize-deepl-v14.svg',
+        'provider' => DeeplBaseSvgIconProvider::class,
+        'source' => 'EXT:deepltranslate_core/Resources/Public/Icons/deepl-mode-aware.svg',
     ],
     'deepl-grey-logo' => [
         'provider' => SvgIconProvider::class,
@@ -20,5 +21,9 @@ return [
     'deepl-logo' => [
         'provider' => SvgIconProvider::class,
         'source' => 'EXT:deepltranslate_core/Resources/Public/Icons/deepl.svg',
+    ],
+    'deepl-logo-mode-aware' => [
+        'provider' => DeeplBaseSvgIconProvider::class,
+        'source' => 'EXT:deepltranslate_core/Resources/Public/Icons/deepl-mode-aware.svg',
     ],
 ];
